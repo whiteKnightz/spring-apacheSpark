e1bbee79d764902464c9430965333b92b14a4b72
Abhishek Satyal <ab.satyal@gmail.com>
2024-02-11 16:13:24 -0500

    feat: init application

---
+++ src/main/java/com/whiteKnightz/spark/springapacheSpark/SpringApacheSparkApplication.java
@@ -0,0 +1,15 @@
+package com.whiteKnightz.spark.springapacheSpark;
+
+import org.apache.spark.sql.SparkSession;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApacheSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApacheSparkApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/whiteKnightz/spark/springapacheSpark/SpringApacheSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiteKnightz.spark.springapacheSpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApacheSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
